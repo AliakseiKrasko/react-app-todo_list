c092aa8c897d1ea3731f27077d6824773fabbe79
AliakseiKrasko <alekskrasko1985@gmail.com>
2024-10-06 17:24:29 +0300

    broken down into components

---
+++ src/Components/header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import classes from './App.css';
+
+const Header = () => {
+    <div className="header">
+        <h1 className="header-title">Список дел</h1>
+        <span className="header-desc">3 осталось, 0 сделано</span>
+    </div>
+}
+
+export default Header;
